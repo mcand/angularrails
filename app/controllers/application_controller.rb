7b169218729d3f4d48fbaf47616c87bd1a20dce4
Andre Furquin <andrefurkin@me.com>
2015-11-30 02:59:29 -0200

    important: accept json request from another host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :json_request?
   before_filter :configure_permitted_parameters, if: :devise_controller?
   helper_method :resource_name, :resource, :devise_mapping
 
@@ -23,5 +24,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:firstname, :lastname, :email, :password, :password_confirmation, :avatar) }
+  end
+
+protected
+  def json_request?
+    request.format.json?
   end
 end
