87f5d45d652090b8bc6f0d83758bc6bc17ad6d91
Andre Furquin <andrefurkin@me.com>
2015-11-30 03:02:52 -0200

    angular movies app module implemented

---
+++ app/assets/javascripts/movies_app.js
@@ -0,0 +1,19 @@
+(function() {
+  var app = angular.module('movies',[
+    'ngRoute',
+    'ngResource',
+    'ngMessages',
+    'templates']);
+
+  app.config(["$routeProvider",
+    function($routeProvider) {
+      $routeProvider.when("/", {
+         controller: "MoviesController",
+        templateUrl: "movies_list.html"
+      }).when("/new", {
+        controller: "MoviesController",
+        templateUrl: "create_movies.html"
+      });
+    }
+  ]);
+})();
